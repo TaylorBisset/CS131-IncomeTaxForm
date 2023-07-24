b98212fdc230d31c4d7af473b125610da0e8cc3b
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-24 11:16:44 -0700

    Comment in program specs from assignment instructions

--- CS131-IncomeTaxForm/CS131-IncomeTaxForm.cpp
+++ CS131-IncomeTaxForm/CS131-IncomeTaxForm.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+/*
+Program Specifications Write a program to calculate U.S. income tax owed given wages, taxable interest, 
+unemployment compensation, status (single or married) and taxes withheld. Taxpayers are only allowed to 
+use this short form if adjusted gross income (AGI) is less than $120000. Dollar amounts are displayed as 
+integers with no comma separators. For example, cout << "Deduction: $" << deduction
+
+Note: this program is designed for incremental development. Complete each step and submit for grading 
+before starting the next step. Only a portion of tests pass after each step but confirm progress.
+*/
+
 int main()
 {
     cout << "Income Tax Form Program\n";
