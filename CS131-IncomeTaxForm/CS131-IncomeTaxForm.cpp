3f022227289fe29e8269eb5d2cdabb0715735b8c
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-24 11:15:27 -0700

    Initial commit

--- CS131-IncomeTaxForm/CS131-IncomeTaxForm.cpp
+++ CS131-IncomeTaxForm/CS131-IncomeTaxForm.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "Income Tax Form Program\n";
 
+
+// End program sequence
+    cout << endl << endl;
+#ifdef _WIN32
+    system("pause");
+#else
+    cout << "Press any key to continue . . . ";
+    cin.get();
+#endif
     return 0;
 }
